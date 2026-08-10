47a6f7445acdaacdf64eb3a71aed566b99f5422a
muhammed sahal <muhammedsahalap717@gmail.com>
2026-08-10 18:58:53 +0530

    feat: implement wishlist slice with add, remove, toggle, and clear actions

--- src/redux/slices/wishlistSlice.js
+++ src/redux/slices/wishlistSlice.js
@@ -8,8 +8,39 @@ const wishlistSlice = createSlice({
   name: 'wishlist',
   initialState,
   reducers: {
-    // Actions to be added later
+    addToWishlist: (state, action) => {
+      const product = action.payload;
+      const existingItem = state.items.find((item) => item.id === product.id);
+      
+      if (!existingItem) {
+        state.items.push(product);
+      }
+    },
+    removeFromWishlist: (state, action) => {
+      const productId = action.payload;
+      state.items = state.items.filter((item) => item.id !== productId);
+    },
+    toggleWishlist: (state, action) => {
+      const product = action.payload;
+      const existingItemIndex = state.items.findIndex((item) => item.id === product.id);
+      
+      if (existingItemIndex >= 0) {
+        state.items.splice(existingItemIndex, 1);
+      } else {
+        state.items.push(product);
+      }
+    },
+    clearWishlist: (state) => {
+      state.items = [];
+    },
   },
 });
+
+export const {
+  addToWishlist,
+  removeFromWishlist,
+  toggleWishlist,
+  clearWishlist,
+} = wishlistSlice.actions;
 
 export default wishlistSlice.reducer;
